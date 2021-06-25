3bf3d3f2fefa4424a0c3cf4fa9974739a38efa94
Tjerand Bjornsen <tjerand.bjornsen@nordicsemi.no>
2021-06-25 09:14:45 +0200

    gas resistance sensor uses zephyrs generic sensor ipso object.

--- samples/nrf9160/lwm2m_demo/src/lwm2m/lwm2m_env_sensor.c
+++ samples/nrf9160/lwm2m_demo/src/lwm2m/lwm2m_env_sensor.c
@@ -14,18 +14,16 @@ LOG_MODULE_REGISTER(app_lwm2m_env_sens, CONFIG_APP_LOG_LEVEL);
 #define ENV_SENSOR_NODE	DT_PATH(soc, peripheral_40000000, i2c_a000, bme680_76)
 #define ENV_SENSOR_NAME	DT_LABEL(ENV_SENSOR_NODE)
 
-#define GAS_SENSOR_APP_NAME "Gas resistance sensor, measures air quality"
-
 /* Default values if sensors not available */
 static struct float32_value temp_float = { 25, 500000 };
 static struct float32_value press_float = { 100, 500000 };
 static struct float32_value humid_float = { 45, 500000 };
-static struct float32_value gas_float = { 1000, 500000 };	// TODO: find valid default value
+static struct float32_value gas_resistance_float = { 1000, 500000 };	// TODO: find plausible default value
 
 static char *temp_unit = "C";
 static char *press_unit = "kPa";
 static char *humid_unit = "%";
-static char *gas_unit = "Ohm";
+static char *gas_resistance_unit = "Ohm";
 
 static const struct device *die_dev;
 static int32_t timestamp;
@@ -148,11 +146,9 @@ static void *humidity_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t re
 	return &humid_float;
 }
 
-static void *gas_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_inst_id,
+static void *gas_resistance_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_inst_id,
 			  size_t *data_len)
 {
-	int32_t ts;
-
 #if defined(ENV_SENSOR_NAME)
 	/*
 	 * No need to check if read was successful, just reuse the
@@ -160,12 +156,12 @@ static void *gas_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_ins
 	 * This is because there is currently no way to report read_cb
 	 * failures to the LWM2M engine.
 	 */
-	read_enviroment_sensor(die_dev, gas_unit, SENSOR_CHAN_GAS_RES, &gas_float);
+	read_enviroment_sensor(die_dev, gas_resistance_unit, SENSOR_CHAN_GAS_RES, &gas_resistance_float);
 #endif
-	lwm2m_engine_set_float32("3325/0/5700", &gas_float);
-	*data_len = sizeof(gas_float);
+	lwm2m_engine_set_float32("3300/0/5700", &gas_resistance_float);
+	*data_len = sizeof(gas_resistance_float);
 
-	return &gas_float;
+	return &gas_resistance_float;
 }
 
 int lwm2m_init_env_sensor(void)
@@ -201,12 +197,13 @@ int lwm2m_init_env_sensor(void)
 	lwm2m_engine_set_res_data("3304/0/5701", humid_unit, sizeof(humid_unit),
 				  LWM2M_RES_DATA_FLAG_RO);
 
-	lwm2m_engine_create_obj_inst("3325/0");
-	lwm2m_engine_register_read_callback("3325/0/5700", gas_read_cb);
-	lwm2m_engine_set_res_data("3325/0/5701", gas_unit, sizeof(gas_unit),
+	lwm2m_engine_create_obj_inst("3300/0");
+	lwm2m_engine_register_read_callback("3300/0/5700", gas_resistance_read_cb);
+	lwm2m_engine_set_res_data("3300/0/5701", gas_resistance_unit, sizeof(gas_resistance_unit),
 				  LWM2M_RES_DATA_FLAG_RO);
-	lwm2m_engine_set_res_data("3325/0/5750",
-                GAS_SENSOR_APP_NAME, sizeof(GAS_SENSOR_APP_NAME),
+	lwm2m_engine_set_res_data("3300/0/5750",
+                CONFIG_LWM2M_IPSO_GENERIC_SENSOR_APP_TYPE, 
+				sizeof(CONFIG_LWM2M_IPSO_GENERIC_SENSOR_APP_TYPE),
                 LWM2M_RES_DATA_FLAG_RO);
 	return 0;
 }
