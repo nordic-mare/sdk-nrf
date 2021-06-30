1cbe27cf486906f3b3966c2e5fdb5b14e0f23f45
Tjerand Bjornsen <tjerand.bjornsen@nordicsemi.no>
2021-06-30 10:34:17 +0200

    stripped down env sensor to prepare for sensor/lwm2m split

--- samples/nrf9160/lwm2m_demo/src/lwm2m/lwm2m_env_sensor.c
+++ samples/nrf9160/lwm2m_demo/src/lwm2m/lwm2m_env_sensor.c
@@ -14,23 +14,20 @@ LOG_MODULE_REGISTER(app_lwm2m_env_sens, CONFIG_APP_LOG_LEVEL);
 #define ENV_SENSOR_NODE	DT_PATH(soc, peripheral_40000000, i2c_a000, bme680_76)
 #define ENV_SENSOR_NAME	DT_LABEL(ENV_SENSOR_NODE)
 
-/* Default values if sensors not available */
-static struct float32_value temp_float = { 25, 500000 };
-static struct float32_value press_float = { 100, 500000 };
-static struct float32_value humid_float = { 45, 500000 };
-static struct float32_value gas_resistance_float = { 1000, 500000 };	// TODO: find plausible default value
+#define TEMP_UNIT "C"
+#define PRESS_UNIT "kPa"
+#define HUMID_UNIT "%"
+#define GAS_RES_UNIT "Ohm"
 
-static char *temp_unit = "C";
-static char *press_unit = "kPa";
-static char *humid_unit = "%";
-static char *gas_resistance_unit = "Ohm";
+static float32_value_t temp_float;
+static float32_value_t press_float;
+static float32_value_t humid_float;
+static float32_value_t gas_resistance_float;	
 
 static const struct device *die_dev;
-static int32_t timestamp;
 
-#if defined(ENV_SENSOR_NAME)
 static int read_enviroment_sensor(const struct device *sens_dev, char *unit,
-                enum sensor_channel channel, struct float32_value *float_val)
+                enum sensor_channel channel, float32_value_t *float_val)
 {
 	const char *name = sens_dev->name;
 	struct sensor_value sensor_val;
@@ -54,34 +51,14 @@ static int read_enviroment_sensor(const struct device *sens_dev, char *unit,
 
 	return 0;
 }
-#endif
 
 static void *temp_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_inst_id,
 			  size_t *data_len)
 {
-	int32_t ts;
+	
+	read_enviroment_sensor(die_dev, TEMP_UNIT, SENSOR_CHAN_AMBIENT_TEMP, &temp_float);
 
-	/* Only object instance 0 is currently used */
-	if (obj_inst_id != 0) {
-		*data_len = 0;
-		return NULL;
-	}
-
-#if defined(ENV_SENSOR_NAME)
-	/*
-	 * No need to check if read was successful, just reuse the
-	 * previous value which is already stored at temp_float.
-	 * This is because there is currently no way to report read_cb
-	 * failures to the LWM2M engine.
-	 */
-	read_enviroment_sensor(die_dev, temp_unit, SENSOR_CHAN_AMBIENT_TEMP, &temp_float);
-#endif
-	lwm2m_engine_set_float32("3303/0/5700", &temp_float);
 	*data_len = sizeof(temp_float);
-	/* get current time from device */
-	lwm2m_engine_get_s32("3/0/13", &ts);
-	/* set timestamp */
-	lwm2m_engine_set_s32("3303/0/5518", ts);
 
 	return &temp_float;
 }
@@ -89,29 +66,10 @@ static void *temp_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_in
 static void *pressure_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_inst_id,
 			  size_t *data_len)
 {
-	int32_t ts;
 
-	/* Only object instance 0 is currently used */
-	if (obj_inst_id != 0) {
-		*data_len = 0;
-		return NULL;
-	}
+	read_enviroment_sensor(die_dev, PRESS_UNIT, SENSOR_CHAN_PRESS, &press_float);
 
-#if defined(ENV_SENSOR_NAME)
-	/*
-	 * No need to check if read was successful, just reuse the
-	 * previous value which is already stored at temp_float.
-	 * This is because there is currently no way to report read_cb
-	 * failures to the LWM2M engine.
-	 */
-	read_enviroment_sensor(die_dev, press_unit, SENSOR_CHAN_PRESS, &press_float);
-#endif
-	lwm2m_engine_set_float32("3323/0/5700", &press_float);
 	*data_len = sizeof(press_float);
-	/* get current time from device */
-	lwm2m_engine_get_s32("3/0/13", &ts);
-	/* set timestamp */
-	lwm2m_engine_set_s32("3323/0/5518", ts);
 
 	return &press_float;
 }
@@ -119,29 +77,10 @@ static void *pressure_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t re
 static void *humidity_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_inst_id,
 			  size_t *data_len)
 {
-	int32_t ts;
 
-	/* Only object instance 0 is currently used */
-	if (obj_inst_id != 0) {
-		*data_len = 0;
-		return NULL;
-	}
+	read_enviroment_sensor(die_dev, HUMID_UNIT, SENSOR_CHAN_HUMIDITY, &humid_float);
 
-#if defined(ENV_SENSOR_NAME)
-	/*
-	 * No need to check if read was successful, just reuse the
-	 * previous value which is already stored at temp_float.
-	 * This is because there is currently no way to report read_cb
-	 * failures to the LWM2M engine.
-	 */
-	read_enviroment_sensor(die_dev, humid_unit, SENSOR_CHAN_HUMIDITY, &humid_float);
-#endif
-	lwm2m_engine_set_float32("3304/0/5700", &humid_float);
 	*data_len = sizeof(humid_float);
-	/* get current time from device */
-	lwm2m_engine_get_s32("3/0/13", &ts);
-	/* set timestamp */
-	lwm2m_engine_set_s32("3304/0/5518", ts);
 
 	return &humid_float;
 }
@@ -149,16 +88,8 @@ static void *humidity_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t re
 static void *gas_resistance_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_inst_id,
 			  size_t *data_len)
 {
-#if defined(ENV_SENSOR_NAME)
-	/*
-	 * No need to check if read was successful, just reuse the
-	 * previous value which is already stored at temp_float.
-	 * This is because there is currently no way to report read_cb
-	 * failures to the LWM2M engine.
-	 */
-	read_enviroment_sensor(die_dev, gas_resistance_unit, SENSOR_CHAN_GAS_RES, &gas_resistance_float);
-#endif
-	lwm2m_engine_set_float32("3300/0/5700", &gas_resistance_float);
+	read_enviroment_sensor(die_dev, GAS_RES_UNIT, SENSOR_CHAN_GAS_RES, &gas_resistance_float);
+
 	*data_len = sizeof(gas_resistance_float);
 
 	return &gas_resistance_float;
@@ -166,11 +97,9 @@ static void *gas_resistance_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint1
 
 int lwm2m_init_env_sensor(void)
 {
-#if defined(ENV_SENSOR_NAME)
 	die_dev = device_get_binding(ENV_SENSOR_NAME);
 	LOG_INF("%s on-die enviroment sensor %s",
 		die_dev ? "Found" : "Did not find", ENV_SENSOR_NAME);
-#endif
 
 	if (!die_dev) {
 		LOG_ERR("No enviroment sensor device found.");
@@ -178,28 +107,22 @@ int lwm2m_init_env_sensor(void)
 
 	lwm2m_engine_create_obj_inst("3303/0");
 	lwm2m_engine_register_read_callback("3303/0/5700", temp_read_cb);
-	lwm2m_engine_set_res_data("3303/0/5518",
-				  &timestamp, sizeof(timestamp), 0);
-	lwm2m_engine_set_res_data("3303/0/5701", temp_unit, sizeof(temp_unit),
+	lwm2m_engine_set_res_data("3303/0/5701", TEMP_UNIT, sizeof(TEMP_UNIT),
 				  LWM2M_RES_DATA_FLAG_RO);
 
     lwm2m_engine_create_obj_inst("3323/0");
 	lwm2m_engine_register_read_callback("3323/0/5700", pressure_read_cb);
-	lwm2m_engine_set_res_data("3323/0/5518",
-				  &timestamp, sizeof(timestamp), 0);
-	lwm2m_engine_set_res_data("3323/0/5701", press_unit, sizeof(press_unit),
+	lwm2m_engine_set_res_data("3323/0/5701", PRESS_UNIT, sizeof(PRESS_UNIT),
 				  LWM2M_RES_DATA_FLAG_RO);
 	
     lwm2m_engine_create_obj_inst("3304/0");
 	lwm2m_engine_register_read_callback("3304/0/5700", humidity_read_cb);
-	lwm2m_engine_set_res_data("3304/0/5518",
-				  &timestamp, sizeof(timestamp), 0);
-	lwm2m_engine_set_res_data("3304/0/5701", humid_unit, sizeof(humid_unit),
+	lwm2m_engine_set_res_data("3304/0/5701", HUMID_UNIT, sizeof(HUMID_UNIT),
 				  LWM2M_RES_DATA_FLAG_RO);
 
 	lwm2m_engine_create_obj_inst("3300/0");
 	lwm2m_engine_register_read_callback("3300/0/5700", gas_resistance_read_cb);
-	lwm2m_engine_set_res_data("3300/0/5701", gas_resistance_unit, sizeof(gas_resistance_unit),
+	lwm2m_engine_set_res_data("3300/0/5701", GAS_RES_UNIT, sizeof(GAS_RES_UNIT),
 				  LWM2M_RES_DATA_FLAG_RO);
 	lwm2m_engine_set_res_data("3300/0/5750",
                 "A measure for air quality", 
