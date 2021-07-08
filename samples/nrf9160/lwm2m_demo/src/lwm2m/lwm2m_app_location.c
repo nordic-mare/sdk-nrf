d6352969929d570e34290c22bce5e08dbc3a1880
Markus Rekdal <markus.rekdal@nordicsemi.no>
2021-07-08 09:13:17 +0200

    Add more useful debug info to GPS Module
    Signed-off-by: Markus Rekdal <markus.rekdal@nordicsemi.no>

--- samples/nrf9160/lwm2m_demo/src/lwm2m/lwm2m_app_location.c
+++ samples/nrf9160/lwm2m_demo/src/lwm2m/lwm2m_app_location.c
@@ -14,7 +14,6 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(lwm2m_app_loc, CONFIG_APP_LOG_LEVEL);
 
-static struct gps_nmea nmea_data;
 static struct gps_pvt pvt_data;
 static const struct device *gps_dev;
 static struct gps_config gps_cfg = {
@@ -24,16 +23,6 @@ static struct gps_config gps_cfg = {
 	.timeout = 150,
 	.priority = false
 };
-
-
-
-static void update_location_data(struct gps_nmea *nmea)
-{
-	LOG_ERR("GPS Data: %s", log_strdup(nmea->buf));
-	/* TODO: change API to send actual values (not string)
-	 * so we can update Location object resources
-	 */
-}
 
 static float32_value_t float_to_lwm2m_float(float val) {
 	float32_value_t out;
@@ -55,13 +44,11 @@ static void handle_pvt_fix(struct gps_pvt *pvt)
 	float32_value_t longitude = double_to_lwm2m_float(pvt->longitude);
 	float32_value_t altitude = float_to_lwm2m_float(pvt->altitude);
 	float32_value_t speed = float_to_lwm2m_float(pvt->speed);
-	int32_t uptime = k_uptime_get_32();
+
 	lwm2m_engine_set_float32("6/0/0", &latitude);
 	lwm2m_engine_set_float32("6/0/1", &longitude);
 	lwm2m_engine_set_float32("6/0/2", &altitude);
 	lwm2m_engine_set_float32("6/0/6", &speed);
-	lwm2m_engine_set_res_data("6/0/5", &uptime,
-				 sizeof(int32_t), LWM2M_RES_DATA_FLAG_RO);
 }
 
 /**@brief Callback for GPS events */
@@ -69,32 +56,42 @@ static void gps_event_handler(const struct device *dev, struct gps_event *evt)
 {
 	ARG_UNUSED(dev);
 	static uint32_t timestamp_prev;
-	if (evt->type == GPS_EVT_PVT_FIX) {
-		LOG_DBG("Recieved PVT Fix");
-		memcpy(&pvt_data, &evt->pvt, sizeof(struct gps_pvt));
-		handle_pvt_fix(&pvt_data);
-		timestamp_prev = k_uptime_get_32();
+	switch (evt->type)
+	{
+		case GPS_EVT_SEARCH_STARTED:
+			LOG_DBG("GPS search started");
+			break;
+		case GPS_EVT_SEARCH_STOPPED:
+			LOG_DBG("GPS search stopped");
+			break;
+		case GPS_EVT_SEARCH_TIMEOUT:
+			LOG_DBG("GPS search timed out");
+			break;
+		case GPS_EVT_PVT:
+			break;
+		case GPS_EVT_PVT_FIX:
+			LOG_DBG("Recieved PVT Fix. GPS search completed.");
+			memcpy(&pvt_data, &evt->pvt, sizeof(struct gps_pvt));
+			handle_pvt_fix(&pvt_data);
+			timestamp_prev = k_uptime_get_32();
+			break;
+		case GPS_EVT_NMEA:
+			break;
+		case GPS_EVT_NMEA_FIX:
+			break;
+		case GPS_EVT_OPERATION_BLOCKED:
+			LOG_DBG("GPS search blocked");
+			break;
+		case GPS_EVT_OPERATION_UNBLOCKED:
+			LOG_DBG("GPS unblocked. Resuming search.");
+			break;
+		case GPS_EVT_AGPS_DATA_NEEDED:
+			LOG_DBG("GPS requests AGPS Data. AGPS is not implemented.");
+			break;
+		case GPS_EVT_ERROR:
+			LOG_DBG("GPS error occured");
+			break;	
 	}
-
-	if (evt->type != GPS_EVT_NMEA_FIX) {
-		return;
-	}
-
-	if (k_uptime_get_32() - timestamp_prev <
-	    CONFIG_APP_HOLD_TIME_GPS * MSEC_PER_SEC) {
-		return;
-	}
-
-	if (gps_dev == NULL) {
-		LOG_ERR("Could not get %s device",
-			log_strdup(CONFIG_GPS_DEV_NAME));
-		return;
-	}
-
-	memcpy(&nmea_data, &evt->nmea, sizeof(struct gps_nmea));
-
-	update_location_data(&nmea_data);
-	timestamp_prev = k_uptime_get_32();
 }
 
 int lwm2m_app_init_location(void)
